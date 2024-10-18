306ae68cdd24a8a916431c24c483abd63f6e0e2c
Dominik Wilkowski <hi@dominik-wilkowski.com>
2024-10-18 14:18:01 +1100

    prepared permissions

--- dev/psql-compose/init.sql
+++ dev/psql-compose/init.sql
@@ -1,18 +1,19 @@
 -- init.sql
 CREATE TABLE users (
-  id         INTEGER PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
-  username   TEXT NOT NULL UNIQUE,
-  password   TEXT NOT NULL,
-  created_at TIMESTAMPTZ DEFAULT CURRENT_TIMESTAMP
+  id                   INTEGER PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
+  username             TEXT NOT NULL UNIQUE,
+  password             TEXT NOT NULL,
+  created_at           TIMESTAMPTZ DEFAULT CURRENT_TIMESTAMP,
+  permission_equipment TEXT NOT NULL,
+  permission_user      TEXT NOT NULL,
+  permission_todo      TEXT NOT NULL,
 );
-INSERT INTO users (username, password) VALUES ('dom', '$argon2id$v=19$m=19456,t=2,p=1$T9GO2wvNWMGcMQ/uPdH8lQ$EjVtyckTRnjly15GvDW3RAo2GvZPT/Dv7prpRDv6YcI'), ('thewizzy', '$argon2id$v=19$m=19456,t=2,p=1$jGpHvsSseOmqYpSjYmHsDw$C/tnsXIf8dEdGojiKDcYis3e7gniaT40jvqIyFzri4c');
+INSERT INTO users
+  (username, password)
+  VALUES
+  ('dom', '$argon2id$v=19$m=19456,t=2,p=1$T9GO2wvNWMGcMQ/uPdH8lQ$EjVtyckTRnjly15GvDW3RAo2GvZPT/Dv7prpRDv6YcI', 'READ(*)', 'READ(*)', 'READ(2)'),
+  ('thewizzy', '$argon2id$v=19$m=19456,t=2,p=1$jGpHvsSseOmqYpSjYmHsDw$C/tnsXIf8dEdGojiKDcYis3e7gniaT40jvqIyFzri4c', 'READ(*)', 'READ(*)', 'WRITE(*)');
 -- password for both: test
-
-CREATE TABLE user_permissions (
-    user_id  INT NOT NULL REFERENCES users(id) ON DELETE CASCADE,
-    token    TEXT NOT NULL
-);
-INSERT INTO user_permissions (user_id, token) VALUES (1, 'READ'), (1, 'WRITE'), (2, 'READ');
 
 CREATE TABLE todos (
   id         INTEGER PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
@@ -21,4 +22,4 @@ CREATE TABLE todos (
   completed  BOOLEAN,
   created_at TIMESTAMPTZ DEFAULT CURRENT_TIMESTAMP
 );
-INSERT INTO todos (user_id, title, completed) VALUES (1, 'Something to do!', false), (2, 'Last thing!', false);
+INSERT INTO todos (user_id, title, completed) VALUES (1, 'Something to do!', false), (2, 'So much todo', false), (1, 'Last thing!', false);
